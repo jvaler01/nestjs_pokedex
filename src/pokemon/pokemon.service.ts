de89b161031e75cb4d733d9d65d07f6282c1c25d
jvaler01 <jorgevm13@gmail.com>
2023-09-28 17:46:31 +0200

    first commit, db config pokemon schema

---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,54 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  findAll() {
+    return `This action returns all pokemon`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} pokemon`;
+  }
+
+  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+    return updatePokemonDto;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pokemon`;
+  }
+  private handleExceptions(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
+      );
+    }
+    console.log(error);
+    throw new InternalServerErrorException(
+      `Can't create Pokemon - Check server logs`,
+    );
+  }
+}
